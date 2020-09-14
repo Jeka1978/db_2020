3e798f2f684052a5f97fc6088230c4660e2eb890
jeka1978 <kit2009>
2020-09-14 13:43:15 +0300

    yest lab Friday 13

---
+++ src/main/java/java_8_examples/Friday13Service.java
@@ -0,0 +1,18 @@
+package java_8_examples;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class Friday13Service {
+    public static void printMap(int startYear, int endYear) {
+
+        //1913 - 3
+        //1917 - 3
+        //1918 - 3
+        //1903 - 2
+        //1904 - 2
+        //1901 - 1
+    }
+}
